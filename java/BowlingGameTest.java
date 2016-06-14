b9a039a8c6a1158090e95739faa4eab445f74100
Jan Henrik Lenes <jhlenes@outlook.com>
2016-06-14 13:44:09 +0000

    Squashed commit of the following:
    commit 1ccd8572f7dfdcd7b81f98f5e2d77945fedeffae
    Merge: 0aad6fe 95465a9
    Author: Jan Henrik Lenes <jhlenes@outlook.com>
    Date:   Tue Jun 14 15:43:15 2016 +0200
    
        Merge branch 'ready/25' of https://github.com/praqma-training/code-academy-phlow-demo into 25
    
    commit 0aad6fee5b0b1b71072015bc360f80390dd2630c
    Author: Jan Henrik Lenes <jhlenes@outlook.com>
    Date:   Tue Jun 14 15:36:40 2016 +0200
    
        Fix #25: Implement second test
    
    commit 95465a93b9e78570871dcbdcced0dc0f3abb904a
    Author: Jan Henrik Lenes <jhlenes@outlook.com>
    Date:   Tue Jun 14 15:36:40 2016 +0200
    
        Fix #25: Implement second test

--- java/BowlingGameTest.java
+++ java/BowlingGameTest.java
@@ -1,11 +1,24 @@
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
+  private Game g;
+
+  protected void setUp() throws Exception {
+    g = new Game();
+  }
+  
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++)
+      g.roll(pins);
+  }
+  
   public void testGutterGame() throws Exception {
-    Game g = new Game();
-    for (int i=0; i<20; i++)
-      g.roll(0);
+    rollMany(20, 0);
     assertEquals(0, g.score());
   }
 
+  public void testAllOnes() throws Exception {
+    rollMany(20,1);
+    assertEquals(20, g.score());
+  }
 }
--- java/Game.java
+++ java/Game.java
@@ -1,9 +1,11 @@
 public class Game {
+  private int score = 0;
+
   public void roll(int pins) {
+    score += pins;
   }
 
   public int score() {
-    return 0;
+    return score;
   }
-
 }
